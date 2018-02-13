ea0936e719f6741ae1103f908b1bf79e04ca95bd
david <d.cornel@tu-braunschweig.de>
2018-02-13 14:58:51 +0100

    First React Template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,7 @@
-console.log('App.js is running!');
+console.log('App.js is running');
+
+//JSX - JavaScript XML
+var template = React.createElement("p", {id: "someId"}, "MyFirstJSX from app.js!");
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
