378a1b2b20256fb51d555a299c9e0b02713aa811
david <d.cornel@tu-braunschweig.de>
2018-02-14 08:59:32 +0100

    Formatted JSX Code to fit convention. Added bracket at start and end of html code for visibility.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 console.log('App.js is running');
 
 //JSX - JavaScript XML
-var template = <div><h1>MyFirstCompiledReact!</h1><p>MyFirstReactParagraph</p></div>;
+var template = (
+    <div>
+        <h1>MyFirstCompiledReact!</h1>
+        <p>MyFirstReactParagraph</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
