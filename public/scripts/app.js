3215ac8a4c136de737f6ba0ef7664adb40493706
david <d.cornel@tu-braunschweig.de>
2018-03-01 15:21:05 +0100

    Implemented VisibilityToggle component. Removed version 1 example comments.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,105 +11,6 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 console.log('App.js is running.');
 var appRoot = document.getElementById('app');
 
-//es6 functions example challenge
-// const multiplier = {
-//     numbers: [1, 2, 3, 4],
-//     multiplyBy: 2,
-//     multiply() {
-//         return this.numbers.map((number) => number * this.multiplyBy);
-//     }
-// };
-// console.log(multiplier.multiply());
-
-//counter example with custom render function
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-//     console.log('addOne');
-// };
-// const subOne = () => {
-//     count--;
-//     renderCounterApp();
-//     console.log('subOne');
-// };
-// const resetCounter = () => {
-//     count = 0;
-//     renderCounterApp();
-//     console.log('resetCounter');
-// };
-// const renderCounterApp = () => {
-//     const templateCounter = (
-//         <div>
-//             <h1>count: {count}</h1>
-//             <button id="count_plus_btn" className="btn" onClick={addOne}>+1</button>
-//             <button id="count_minus_btn" className="btn" onClick={subOne}>-1</button>
-//             <button id="count_reset_btn" className="btn" onClick={resetCounter}>reset</button>
-//         </div>
-//     );
-//     ReactDOM.render(templateCounter, appRoot);
-// };
-//
-// renderCounterApp();
-
-
-//JSX - JavaScript XML
-// const app = {
-//     title: 'My first compiled react site',
-//     subtitle: 'List',
-//     options: ['Item One', 'Item Two']
-// };
-// const randomOption = () => {
-//     const randomNo = Math.floor(Math.random() * app.options.length);
-//     alert(app.options[randomNo]);
-// }
-// const renderOptions = () => {
-//     const template = (
-//         <div>
-//             <h1>{app.title}</h1>
-//             {app.subtitle && <h2>{app.subtitle}</h2>}
-//             <p>{(app.options.length > 0) ? 'Here are your options!' : 'No options!'}</p>
-//             <p>Total options: {app.options.length}</p>
-//             <button onClick={removeAllOptions}>Clear options</button>
-//             <form onSubmit={onOptionFormSubmit}>
-//                 <input type="text" name="option"/>
-//                 <button>Add option</button>
-//             </form>
-//             <button disabled={app.options.length === 0} onClick={randomOption}>Choose option</button>
-//             <ol>
-//                 {
-//                     app.options.map((option) => {
-//                         return <li key={option}>{option}</li>;
-//                     })
-//                 }
-//             </ol>
-//         </div>
-//     );
-//     ReactDOM.render(template, appRoot);
-// }
-// const onOptionFormSubmit = (e) => {
-//     e.preventDefault();
-//     const option = e.target.elements.option.value;
-//     if(option == null || option === '') {
-//         console.log('Type out option to be added.');
-//         return;
-//     }
-//     app.options.push(option);
-//     e.target.elements.option.value = '';
-//     renderOptions();
-//     console.log('Option added. Total options = ' + app.options.length + '.');
-//     return;
-// }
-// const removeAllOptions = () => {
-//     app.options = [];
-//     renderOptions();
-//     console.log('All options removed.');
-//     return;
-// }
-//
-// renderOptions();
-
-
 //visibility toggle app
 // let visibilityStatus = false;
 // const renderVisibilityApp = () => {
@@ -404,4 +305,52 @@ var Counter = function (_React$Component7) {
     return Counter;
 }(React.Component);
 
-ReactDOM.render(React.createElement(Counter, null), appRoot);
+//Visibility toggle example 02
+
+
+var VisibilityToggle = function (_React$Component8) {
+    _inherits(VisibilityToggle, _React$Component8);
+
+    function VisibilityToggle(props) {
+        _classCallCheck(this, VisibilityToggle);
+
+        var _this8 = _possibleConstructorReturn(this, (VisibilityToggle.__proto__ || Object.getPrototypeOf(VisibilityToggle)).call(this, props));
+
+        _this8.state = {
+            isVisible: false
+        };
+        _this8.toggleVisibility = _this8.toggleVisibility.bind(_this8);
+        return _this8;
+    }
+
+    _createClass(VisibilityToggle, [{
+        key: 'toggleVisibility',
+        value: function toggleVisibility() {
+            this.setState(function (prevState) {
+                return { isVisible: !prevState.isVisible };
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'button',
+                    { onClick: this.toggleVisibility },
+                    this.state.isVisible ? 'Hide.' : 'Show.'
+                ),
+                this.state.isVisible && React.createElement(
+                    'p',
+                    null,
+                    'Text is shown'
+                )
+            );
+        }
+    }]);
+
+    return VisibilityToggle;
+}(React.Component);
+
+ReactDOM.render(React.createElement(VisibilityToggle, null), appRoot);
--- src/app.js
+++ src/app.js
@@ -1,115 +1,6 @@
 console.log('App.js is running.');
 const appRoot = document.getElementById('app');
 
-//es6 functions example challenge
-// const multiplier = {
-//     numbers: [1, 2, 3, 4],
-//     multiplyBy: 2,
-//     multiply() {
-//         return this.numbers.map((number) => number * this.multiplyBy);
-//     }
-// };
-// console.log(multiplier.multiply());
-
-//counter example with custom render function
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-//     console.log('addOne');
-// };
-// const subOne = () => {
-//     count--;
-//     renderCounterApp();
-//     console.log('subOne');
-// };
-// const resetCounter = () => {
-//     count = 0;
-//     renderCounterApp();
-//     console.log('resetCounter');
-// };
-// const renderCounterApp = () => {
-//     const templateCounter = (
-//         <div>
-//             <h1>count: {count}</h1>
-//             <button id="count_plus_btn" className="btn" onClick={addOne}>+1</button>
-//             <button id="count_minus_btn" className="btn" onClick={subOne}>-1</button>
-//             <button id="count_reset_btn" className="btn" onClick={resetCounter}>reset</button>
-//         </div>
-//     );
-//     ReactDOM.render(templateCounter, appRoot);
-// };
-//
-// renderCounterApp();
-
-
-
-
-
-
-
-
-//JSX - JavaScript XML
-// const app = {
-//     title: 'My first compiled react site',
-//     subtitle: 'List',
-//     options: ['Item One', 'Item Two']
-// };
-// const randomOption = () => {
-//     const randomNo = Math.floor(Math.random() * app.options.length);
-//     alert(app.options[randomNo]);
-// }
-// const renderOptions = () => {
-//     const template = (
-//         <div>
-//             <h1>{app.title}</h1>
-//             {app.subtitle && <h2>{app.subtitle}</h2>}
-//             <p>{(app.options.length > 0) ? 'Here are your options!' : 'No options!'}</p>
-//             <p>Total options: {app.options.length}</p>
-//             <button onClick={removeAllOptions}>Clear options</button>
-//             <form onSubmit={onOptionFormSubmit}>
-//                 <input type="text" name="option"/>
-//                 <button>Add option</button>
-//             </form>
-//             <button disabled={app.options.length === 0} onClick={randomOption}>Choose option</button>
-//             <ol>
-//                 {
-//                     app.options.map((option) => {
-//                         return <li key={option}>{option}</li>;
-//                     })
-//                 }
-//             </ol>
-//         </div>
-//     );
-//     ReactDOM.render(template, appRoot);
-// }
-// const onOptionFormSubmit = (e) => {
-//     e.preventDefault();
-//     const option = e.target.elements.option.value;
-//     if(option == null || option === '') {
-//         console.log('Type out option to be added.');
-//         return;
-//     }
-//     app.options.push(option);
-//     e.target.elements.option.value = '';
-//     renderOptions();
-//     console.log('Option added. Total options = ' + app.options.length + '.');
-//     return;
-// }
-// const removeAllOptions = () => {
-//     app.options = [];
-//     renderOptions();
-//     console.log('All options removed.');
-//     return;
-// }
-//
-// renderOptions();
-
-
-
-
-
-
 //visibility toggle app
 // let visibilityStatus = false;
 // const renderVisibilityApp = () => {
@@ -262,4 +153,28 @@ class Counter extends React.Component {
     }
 }
 
-ReactDOM.render(<Counter/>, appRoot);
+//Visibility toggle example 02
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isVisible: false
+        };
+        this.toggleVisibility = this.toggleVisibility.bind(this);
+    }
+    toggleVisibility() {
+        this.setState((prevState) => {
+            return {isVisible: !prevState.isVisible};
+        });
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.toggleVisibility}>{this.state.isVisible ? 'Hide.' : 'Show.'}</button>
+                {this.state.isVisible && <p>Text is shown</p>}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<VisibilityToggle/>, appRoot);
