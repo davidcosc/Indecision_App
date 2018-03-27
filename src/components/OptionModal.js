1ed29e436940e0cb7b9ff4bc7daa711214c37a56
david <d.cornel@tu-braunschweig.de>
2018-03-28 01:06:25 +0200

    Added onRequestClose attribute to Modal to trigger clearSelectedOption.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 
 const OptionModal = (props) => {
     return (
-        <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
+        <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option" onRequestClose={props.clearSelectedOption}>
             <h1>Go with option {props.selectedOption}.</h1>
             <button onClick={props.clearSelectedOption}>Close.</button>
         </Modal>
