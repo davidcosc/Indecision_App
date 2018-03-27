4fa30476b2e43308673de61502f7f7b325213171
david <d.cornel@tu-braunschweig.de>
2018-03-28 00:59:21 +0200

    Added contendLabel attribute to Modal since required.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 
 const OptionModal = (props) => {
     return (
-        <Modal isOpen={!!props.selectedOption}>
+        <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
             <h1>Go with option {props.selectedOption}.</h1>
             <button onClick={props.closeOptionModal}>Close.</button>
         </Modal>
