5ecad0b5924826b703dfb46d751f20baf1f8fb94
david <d.cornel@tu-braunschweig.de>
2018-04-05 10:22:27 +0200

    Added AddOption component and inserted it into IndecisionApp.

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,4 +1,21 @@
+import React from 'react';
 
+export default class AddOption extends React.Component {
+    state = {
+        error: undefined,
+    };
+    render() {
+        return (
+            <div>
+                {!!this.state.error && <p>{this.state.error}</p>}
+                <form>
+                    <input type="text" name="option"/>
+                    <button>Add option.</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 
 
