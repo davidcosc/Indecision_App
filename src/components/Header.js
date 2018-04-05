5c997f3af06acd720b615e60412ab148d2f79708
david <d.cornel@tu-braunschweig.de>
2018-04-05 09:03:42 +0200

    Added Header component and inserted it into IndecisionApp.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,63 @@
 import React from 'react';
 
-const Header = (props) => {
-    return (
-        <div>
-            <h1>{props.title}</h1>
-            {props.subtitle && <h2>{props.subtitle}</h2>}
-        </div>
-    );
-};
+const Header = (props) => (
+    <div>
+        <h1>{props.title}</h1>
+        {!!props.title && <h2>{props.subtitle}</h2>}
+    </div>
+);
 Header.defaultProps = {
-    title: 'Indecision App v4.',
+    title: 'IndecisionApp v5.0.',
     subtitle: 'Put your life in the hands of a computer.',
 };
 export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+//
+// const Header = (props) => {
+//     return (
+//         <div>
+//             <h1>{props.title}</h1>
+//             {props.subtitle && <h2>{props.subtitle}</h2>}
+//         </div>
+//     );
+// };
+// Header.defaultProps = {
+//     title: 'Indecision App v4.',
+//     subtitle: 'Put your life in the hands of a computer.',
+// };
+// export default Header;
