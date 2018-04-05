4b3230c1d3a2fea65bf786fb9aed8a6562685364
david <d.cornel@tu-braunschweig.de>
2018-04-05 09:49:28 +0200

    Added OptionModal component. Added selectedOption to IndecisionApp state object. Added clearSelectedOption method to IndecisionApp for OptionModal. Made related changes to pickOption method.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import Header from './Header';
 import Action from './Action';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
     state = {
         options: ['1', '2'],
+        selectedOption: undefined,
     };
     pickOption = () => {
         const optionIndex = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[optionIndex];
-        alert(`Go with option ${option}`);
+        this.setState(() => ({selectedOption: option,}));
     };
     clearOptions = () => {
         this.setState(() => ({options: [],}));
+    };
+    clearSelectedOption = () => {
+        this.setState(() => ({selectedOption: undefined,}));
     };
     render() {
         return (
             <div>
                 <Header/>
                 <Action hasOption={this.state.options.length > 0} pickOption={this.pickOption} clearOptions={this.clearOptions}/>
+                <OptionModal selectedOption={this.state.selectedOption} clearSelectedOption={this.clearSelectedOption}/>
             </div>
         );
     }
