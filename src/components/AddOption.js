55c51668dd03a3f5d5ddd519d200e803671b384f
david <d.cornel@tu-braunschweig.de>
2018-04-05 10:29:09 +0200

    Added handleOnSubmitOption method to AddOption.

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -4,11 +4,17 @@ export default class AddOption extends React.Component {
     state = {
         error: undefined,
     };
+    handleOnSubmitOption = (e) => {
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+        //const error = this.props.addOption(option);
+        e.target.elements.option.value = '';
+    };
     render() {
         return (
             <div>
                 {!!this.state.error && <p>{this.state.error}</p>}
-                <form>
+                <form onSubmit={this.handleOnSubmitOption}>
                     <input type="text" name="option"/>
                     <button>Add option.</button>
                 </form>
