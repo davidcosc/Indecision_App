55c43a2afa63c1693e7c206ea3b85307f6f881f5
david <d.cornel@tu-braunschweig.de>
2018-04-16 11:02:22 +0200

    Added Action component. Changed: IndecisionApp.js, Action.js.

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,5 +1,15 @@
+import React from 'react';
 
-
+const Action = (props) => (
+    <div>
+        <button disabled={!props.hasOption} onClick={props.pickOption}>Pick option.</button>
+        <button disabled={!props.hasOption} onClick={props.clearOptions}>Clear options.</button>
+    </div>
+);
+Action.defaultProps = {
+    hasOption: false,
+};
+export default Action;
 
 
 
