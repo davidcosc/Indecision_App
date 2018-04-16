23bee69f5726d2fdbd4c895dc7c0674b741e8c1d
david <d.cornel@tu-braunschweig.de>
2018-04-16 13:07:38 +0200

    Added componentDidMount and componentDidUpdate method. Also removed comments. Changed: all .js files.

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,55 +3,8 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
-        {props.options.length === 0 && <p>Enter some options to begin.</p>}
+        {props.options.length === 0 && <p>Enter some options to get started.</p>}
         {props.options.map((option) => <Option key={option} option={option} removeOption={props.removeOption}/>)}
     </div>
 );
 export default Options;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import Option from './Option';
-//
-// const Options = (props) => (
-//     <div>
-//         {(props.options.length === 0) && <p>Enter a few options to get started.</p>}
-//         {props.options.map((option) => <Option key={option} option={option} removeOption={props.removeOption}/>)}
-//     </div>
-// );
-// Options.defaultProps = {
-//     options: [],
-// };
-// export default Options;
