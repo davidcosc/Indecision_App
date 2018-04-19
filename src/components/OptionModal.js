4c2d4c4d4a874ba1e3bd0efa2d7a25ad10695c68
david <d.cornel@tu-braunschweig.de>
2018-04-19 10:46:24 +0200

    Added modal scss.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-    <Modal isOpen={!!props.selectedOption} contentLabel="SelectedOptionModal" onRequestClose={props.closeSelectedOptionModal} ariaHideApp={false}>
-        <h2>Go with option {props.selectedOption}</h2>
-        <button onClick={props.closeSelectedOptionModal}>Close.</button>
+    <Modal className="modal" isOpen={!!props.selectedOption} contentLabel="SelectedOptionModal" onRequestClose={props.closeSelectedOptionModal} ariaHideApp={false} closeTimeoutMs={2000}>
+        <h2 className="modal__title">Go with option:</h2>
+        <p className="modal__body">{props.selectedOption}</p>
+        <button className="button" onClick={props.closeSelectedOptionModal}>Close.</button>
     </Modal>
 );
 export default OptionModal;
